c53fde9737afca825a1582ddfe5509e12b0bbed1
Dakota Hernandez <dhernan881@gmail.com>
2021-05-06 21:20:09 -0400

    Added new background and win scenes

---
+++ Ping1000 Final Game/Assets/UI/TextMeshLayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class TextMeshLayer : MonoBehaviour
+{
+    public string layerToPushTo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.sortingLayerName = layerToPushTo;
+        renderer.sortingOrder = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
